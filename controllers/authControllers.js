23bf43796e9db87ffce0fd0298d88e12eaf2803c
Deepak Kumar <116838434+dk041102@users.noreply.github.com>
2026-02-28 16:12:09 +0530

    Update comments in authControllers.js for clarity

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,14 +1,14 @@
 const User = require('../models/User');
 const jwt = require('jsonwebtoken');
 
-// Generate JWT
+// for Generating JWT 
 const generateToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
     expiresIn: '30d',
   });
 };
 
-// 🔐 Register User
+// Register User
 const registerUser = async (req, res) => {
   const { name, email, password } = req.body;
 
@@ -36,7 +36,7 @@ const registerUser = async (req, res) => {
   }
 };
 
-// 🔑 Login User
+//  Login User
 const loginUser = async (req, res) => {
   const { email, password } = req.body;
 
